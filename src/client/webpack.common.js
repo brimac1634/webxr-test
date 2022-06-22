bd27d9d2537cfd9cdf9aa7153edf3f7b4622aa95
Brian MacPherson <brimac1634@gmail.com>
2022-06-22 18:57:02 +0800

    initial commit

---
+++ src/client/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/client.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|glb)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, '../client/assets'),
+                    to: path.resolve(__dirname, '../../docs/client/assets')
+                }
+            ]
+        }),
+    ],
+    resolve: {
+        alias: {
+            three: path.resolve('./node_modules/three')
+        },
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../../docs/client'),
+    }
+};
